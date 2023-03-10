fcce9d3606a71eb7cd15d3c38e5a5987bdd2b6eb
josedavidnup <josedavidnup@gmail.com>
2023-03-10 15:25:03 -0400

    layout for mobile done

---
+++ src/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav></nav>
+      <div id="tabBar"></div>
+      <section>
+        <div id="home"></div>
+        <div id="recomendados"></div>
+        <div id="rentas_destacadas"></div>
+      </section>
+    </>
+  );
+};
+
+export default NavBar;
